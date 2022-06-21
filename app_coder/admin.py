f4e34c2c61e62ec26e42f1e7835bf9993f4c73fe
Pablo Rodriguez <blorodriguez95@gmail.com>
2022-06-21 15:45:43 -0300

    Cambios en admin y forms

--- app_coder/admin.py
+++ app_coder/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from app_coder.models import Farmacia, Cliente, Farmaceutico, Medicamento
+from app_coder.models import Farmacia, Cliente, Farmaceutico, Medicamento, Avatar
 
 admin.site.register(Farmacia)
 
@@ -9,3 +9,5 @@ admin.site.register(Cliente)
 admin.site.register(Farmaceutico)
 
 admin.site.register(Medicamento)
+
+admin.site.register(Avatar)
