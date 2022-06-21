3d787c43c117c53362697ac43b63f2c9d6ec27ce
Pablo Rodriguez <blorodriguez95@gmail.com>
2022-06-21 15:51:22 -0300

    Cambios en views, se agregaron nuevas

--- app_coder/views.py
+++ app_coder/views.py
@@ -1,13 +1,31 @@
+import os
+import random
+import string
 from django.shortcuts import render
 from django.db.models import Q
 from django.forms.models import model_to_dict
+from django.contrib.auth.decorators import login_required
 
-from app_coder.models import Farmacia, Cliente, Farmaceutico, Medicamento
-from app_coder.forms import FarmaciaForm, FarmaceuticoForm, MedicamentoForm, ClienteForm
+from app_coder.models import Farmacia, Cliente, Farmaceutico, Medicamento, Avatar
+from app_coder.forms import FarmaciaForm, FarmaceuticoForm, MedicamentoForm, ClienteForm, AvatarForm
+
+from django.contrib import messages
 
 
 def index(request):
-    return render(request, "app_coder/home.html")
+    avatar_ctx = get_avatar_url_ctx(request)
+    context_dict = {**avatar_ctx}
+    return render(
+        request=request,
+        context=context_dict,
+        template_name="app_coder/home.html"
+    )
+
+def get_avatar_url_ctx(request):
+    avatars = Avatar.objects.filter(user=request.user.id)
+    if avatars.exists():
+        return {"url": avatars[0].image.url}
+    return {}
 
 
 def farmaceuticos(request):
@@ -117,7 +135,7 @@ def farmacia_forms_django(request):
         template_name='app_coder/farmacia_django_forms.html'
     )
 
-
+@login_required
 def farmaceutico_forms_django(request):
     if request.method == 'POST':
         farmaceutico_form = FarmaceuticoForm(request.POST)
@@ -149,6 +167,63 @@ def farmaceutico_forms_django(request):
         template_name='app_coder/farmaceutico_django_forms.html'
     )
 
+@login_required
+def update_farmaceutico(request, pk: int):
+    farmaceutico = Farmaceutico.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        farmaceutico_form = FarmaceuticoForm(request.POST)
+        if farmaceutico_form.is_valid():
+            data = farmaceutico_form.cleaned_data
+            farmaceutico.name = data['name']
+            farmaceutico.matricula = data['matricula']
+            farmaceutico.save()
+
+            farmaceuticos = Farmaceutico.objects.all()
+            context_dict = {
+                'farmaceuticos': farmaceuticos
+            }
+            return render(
+                request=request,
+                context=context_dict,
+                template_name="app_coder/farmaceuticos.html"
+            )
+
+    farmaceutico_form = FarmaceuticoForm(model_to_dict(farmaceutico))
+    context_dict = {
+        'farmaceutico': farmaceutico,
+        'farmaceutico_form': farmaceutico_form,
+    }
+    return render(
+        request=request,
+        context=context_dict,
+        template_name='app_coder/farmaceutico_form.html'
+    )
+
+@login_required
+def delete_farmaceutico(request, pk: int):
+    farmaceutico = Farmaceutico.objects.get(pk=pk)
+    if request.method == 'POST':
+        farmaceutico.delete()
+
+        farmaceuticos = Farmaceutico.objects.all()
+        context_dict = {
+            'farmaceuticos': farmaceuticos
+        }
+        return render(
+            request=request,
+            context=context_dict,
+            template_name="app_coder/farmaceuticos.html"
+        )
+
+    context_dict = {
+        'farmaceutico': farmaceutico,
+    }
+    return render(
+        request=request,
+        context=context_dict,
+        template_name='app_coder/farmaceutico_confirm_delete.html'
+    )
 
 def medicamento_forms_django(request):
     if request.method == 'POST':
@@ -213,7 +288,8 @@ def cliente_forms_django(request):
     )
 
 def search(request):
-    context_dict = dict()
+    avatar_ctx = get_avatar_url_ctx(request)
+    context_dict = {**avatar_ctx}
     if request.GET['all_search']:
         search_param = request.GET['all_search']
         query = Q(name__contains=search_param)
@@ -227,4 +303,147 @@ def search(request):
         request=request,
         context=context_dict,
         template_name="app_coder/home.html",
+    )
+
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class FarmaciaListView(ListView):
+    model = Farmacia
+    template_name = "app_coder/farmacia_list.html"
+
+
+class FarmaciaDetailView(DetailView):
+    model = Farmacia
+    template_name = "app_coder/farmacia_detail.html"
+
+
+class FarmaciaCreateView(CreateView):
+    model = Farmacia
+    success_url = reverse_lazy('app_coder:farmacia-list')
+    fields = ['name', 'address', 'phonenumber']
+
+
+class FarmaciaUpdateView(UpdateView):
+    model = Farmacia
+    success_url = reverse_lazy('app_coder:farmacia-list')
+    fields = ['name', 'address', 'phonenumber']
+
+
+class FarmaciaDeleteView(DeleteView):
+    model = Farmacia
+    success_url = reverse_lazy('app_coder:farmacia-list')
+
+
+from django.shortcuts import redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from app_coder.forms import UserRegisterForm, UserEditForm
+
+from django.contrib.auth.decorators import login_required
+
+
+def register(request):
+    if request.method == 'POST':
+        # form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+
+            form.save()
+            return render(
+                request=request,
+                context={"mensaje": "Usuario Registrado satisfactoriamente."},
+                template_name="app_coder/login.html",
+            )
+    # form = UserCreationForm()
+    else:
+        form = UserRegisterForm() #Aca meti el else
+    return render(
+        request=request,
+        context={"form":form},
+        template_name="app_coder/register.html",
+    )
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                template_name = "app_coder/home.html"
+        else:
+            template_name = "app_coder/login.html"
+        return render(
+            request=request,
+            context={'form': form},
+            template_name=template_name,
+        )
+
+    form = AuthenticationForm()
+    return render(
+        request=request,
+        context={'form': form},
+        template_name="app_coder/login.html",
+    )
+
+
+def logout_request(request):
+      logout(request)
+      return redirect("app_coder:Home")
+
+@login_required
+def user_update(request):
+    user = request.user
+    if request.method == 'POST':
+        form = UserEditForm(request.POST)
+        if form.is_valid():
+            informacion = form.cleaned_data
+            user.first_name = informacion['first_name']
+            user.last_name = informacion['last_name']
+            user.email = informacion['email']
+            user.password1 = informacion['password1']
+            user.password2 = informacion['password2']
+            user.save()
+
+            return redirect('app_coder:Home')
+
+    form = UserEditForm(model_to_dict(user))
+    return render(
+        request=request,
+        context={'form': form},
+        template_name="app_coder/user_form.html",
+    )
+
+
+@login_required
+def avatar_load(request):
+    if request.method == 'POST':
+        form = AvatarForm(request.POST, request.FILES)
+        if form.is_valid  and len(request.FILES) != 0:
+            image = request.FILES['image']
+            avatars = Avatar.objects.filter(user=request.user.id)
+            if not avatars.exists():
+                avatar = Avatar(user=request.user, image=image)
+            else:
+                avatar = avatars[0]
+                if len(avatar.image) > 0:
+                    os.remove(avatar.image.path)
+                avatar.image = image
+            avatar.save()
+            messages.success(request, "Imagen cargada exitosamente")
+            return redirect('app_coder:Home')
+
+    form = AvatarForm()
+    return render(
+        request=request,
+        context={"form": form},
+        template_name="app_coder/avatar_form.html",
     )
