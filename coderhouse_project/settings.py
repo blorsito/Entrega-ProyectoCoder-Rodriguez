30d6c1dfb74804ff470374a9c0f845d558a0cb4c
Pablo Rodriguez <blorodriguez95@gmail.com>
2022-06-22 15:16:19 -0300

    Mejoras

--- coderhouse_project/settings.py
+++ coderhouse_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('f+uaibyblqy8!wvfv@us!y=6%_5!56d8wb1k6cuycrqk$&r608')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
